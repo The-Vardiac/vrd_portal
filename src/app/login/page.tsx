d6674914e7d538b5e549f1dcbbda6bd192538bfe
DL William <dlprogrammer18@gmail.com>
2024-01-18 23:21:40 +0800

    feat: (WIP) login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h3>Test</h3>
+            <h2>Test</h2>
+        </main>
+    )
+}
