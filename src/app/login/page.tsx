579cf5d7eb88438cf339778cde8c64717c70bddb
DL William <dlprogrammer18@gmail.com>
2024-02-07 01:36:38 +0800

    feat: (WIP) login view

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,13 @@
 export default function Login() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h3>Test</h3>
-            <h2>Test</h2>
-        </main>
+        <section className="h-screen flex flex-col w-full">
+            <div className="border border-solid border-black flex flex-col w-1/3 p-20 mx-auto justify-center">
+                <h3 className="mx-auto">VRD Portal</h3>
+                <hr />
+                <p className="mx-auto mt-2">
+                    login with
+                </p>
+            </div>
+        </section>
     )
 }
